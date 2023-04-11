eb33670535d730f4f9ccb82437d7c8e6b5a87c2b
Alejandro Quiroz Carmona <alejandroqc03@outlook.com>
2023-04-11 14:42:55 -0600

    Replaced link creation mechanism with a pure java based one

--- src/com/aquirozc/shorcutsmanager/util/Linker.java
+++ src/com/aquirozc/shorcutsmanager/util/Linker.java
@@ -1,42 +1,74 @@
 package com.aquirozc.shorcutsmanager.util;
 
+import com.aquirozc.shorcutsmanager.init.ShortcutsManager;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Linker {
 
 
-    private String linkTaget = "/Applications/";
+    private String symLinkTarget = "/Applications/";
 
     public Linker(){
-
+        super();
     }
 
-    public void createShorcuts (Application application){
+    public void createShortcut (Application application){
 
-        if (application.getWillCreateSchorcutStatus()){
+        String applicationPackageName = application.getApplicationPackageName();
+
+        if (application.willCreateShortcut()){
 
             try {
-                ProcessBuilder linkProcess = new ProcessBuilder("/bin/zsh","-c", "ln -s \"" + application.getApplicationPackagePath() + "\" \"" + linkTaget + "\"");
-                linkProcess.start();
-                System.out.println("Done");
+
+                Path itemPath = Paths.get(application.getApplicationPackagePath());
+                Path targetPath = Paths.get(symLinkTarget + applicationPackageName);
+
+                Files.createSymbolicLink(targetPath,itemPath);
 
             } catch (IOException e){
+
+                Logger.getLogger(ShortcutsManager.LOG_TAG).log(Level.SEVERE,"Failed to create shortcut for " + applicationPackageName + " on " + symLinkTarget);
 
             }
         }
 
     }
 
-    public void deleteShorcuts (Application application){
+    public void deleteShortcut(Application application){
 
-        if (application.getWillCreateSchorcutStatus()){
+        String applicationPackageName = application.getApplicationPackageName();
+
+        if (application.willCreateShortcut()){
 
             try {
-                ProcessBuilder linkProcess = new ProcessBuilder("/bin/zsh","-c", "rm \"" + linkTaget + application.getApplicationPackageName() + "\"");
-                linkProcess.start();
-                System.out.println("Done");
 
-            } catch (IOException e){
+                File link = new File(symLinkTarget + application.getApplicationPackageName());
+
+                if(link.exists()){
+
+                    link.delete();
+
+                }else{
+
+                    throw new FileNotFoundException();
+
+                }
+
+
+            } catch (SecurityException e){
+
+                Logger.getLogger(ShortcutsManager.LOG_TAG).log(Level.SEVERE,"Failed to delete existing shortcut on " + symLinkTarget +". Access denied.");
+
+            } catch (FileNotFoundException e){
+
+                Logger.getLogger(ShortcutsManager.LOG_TAG).log(Level.SEVERE,"Failed to delete existing shortcut. Shortcut for " + applicationPackageName + " does not exist.");
 
             }
         }
