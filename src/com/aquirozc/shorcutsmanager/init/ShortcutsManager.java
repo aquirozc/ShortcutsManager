b392f1f49eb31a8d54ee3fe46131c6cd8d552f79
Alejandro Quiroz Carmona <alejandroqc03@outlook.com>
2023-04-24 07:09:34 -0600

    Added platform check

--- src/com/aquirozc/shorcutsmanager/init/ShortcutsManager.java
+++ src/com/aquirozc/shorcutsmanager/init/ShortcutsManager.java
@@ -1,13 +1,22 @@
 package com.aquirozc.shorcutsmanager.init;
 
 import com.aquirozc.shorcutsmanager.controller.Controller;
+import com.aquirozc.shorcutsmanager.userinterface.IncompatiblePlatformAlert;
 
 public class ShortcutsManager {
 
     public static final String LOG_TAG = "com.aquirozc.shortcutsmanager";
     public static void main(String[] args) {
 
+        if(!System.getProperty("os.name").startsWith("Mac OS X")){
+            new IncompatiblePlatformAlert().displayAlert();
+            return;
+        }
+
         System.setProperty( "apple.awt.application.appearance", "system" );
+        System.setProperty( "apple.laf.useScreenMenuBar", "true" );
+        System.setProperty("apple.awt.application.name", "Shortcuts Manager Alpha");
+
         Controller controller = new Controller();
         controller.onCreate();
     }
