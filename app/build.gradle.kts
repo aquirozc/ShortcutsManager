7d2ca41396dcf29e2a3add11a5bee2ce2524f685
Alejandro Quiroz Carmona <alejandroqc03@outlook.com>
2023-05-06 11:50:08 -0600

    Set JDK target to 19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
+        languageVersion.set(JavaLanguageVersion.of(19))
     }
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
