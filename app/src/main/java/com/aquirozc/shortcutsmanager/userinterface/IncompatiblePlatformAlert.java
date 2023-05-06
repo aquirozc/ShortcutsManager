6c7822439c571be65dd630382e95429dbc1e35cd
Alejandro Quiroz Carmona <alejandroqc03@outlook.com>
2023-05-06 11:48:18 -0600

    Bumped shadow plugin to 8.1.1 version

--- app/src/main/java/com/aquirozc/shortcutsmanager/userinterface/IncompatiblePlatformAlert.java
+++ app/src/main/java/com/aquirozc/shortcutsmanager/userinterface/IncompatiblePlatformAlert.java
@@ -1,6 +1,14 @@
 package com.aquirozc.shortcutsmanager.userinterface;
 
+import com.aquirozc.shortcutsmanager.init.ShortcutsManager;
+import com.aquirozc.shortcutsmanager.util.BufferedIcnsImageFactory;
+
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 
@@ -9,12 +17,15 @@ public class IncompatiblePlatformAlert extends JFrame {
     private GridBagConstraints layoutConstraints;
     private JLabel errorTittle;
     private JLabel errorMessage;
+    private JLabel imageBanner;
     private JLabel osInfo;
     public IncompatiblePlatformAlert(){
 
-        super("Operating system not soported");
+        super("Operating system not supported");
 
         layoutConstraints = new GridBagConstraints();
+        getContentPane().setBackground(DesignPallete.COLOR_DARK_BUTTON_BACKGROUND_B);
+        getContentPane().setLayout(new GridBagLayout());
 
         errorTittle = new JLabel("Incompatible platform");
         errorTittle.setFont(DesignPallete.FONT_TITLE_B);
@@ -28,8 +39,17 @@ public class IncompatiblePlatformAlert extends JFrame {
         osInfo.setFont(DesignPallete.FONT_BUTTON_A);
         osInfo.setForeground(Color.WHITE);
 
-        getContentPane().setBackground(DesignPallete.COLOR_DARK_BUTTON_BACKGROUND_B);
-        getContentPane().setLayout(new GridBagLayout());
+        BufferedImage ogImage = null;
+
+        try {
+            ogImage = new BufferedIcnsImageFactory().getResizedBufferedImage(ImageIO.read(getClass().getClassLoader().getResource("SupportedPlatforms.png")),384,210);
+        } catch (IOException e) {
+
+            Logger.getLogger(ShortcutsManager.LOG_TAG).log(Level.SEVERE,"Failed to load image banner from resources folder");
+
+        }
+
+        imageBanner = new JLabel(new ImageIcon(ogImage));
 
         layoutConstraints.gridx = 0;
         layoutConstraints.anchor= GridBagConstraints.LINE_START;
@@ -43,14 +63,20 @@ public class IncompatiblePlatformAlert extends JFrame {
         layoutConstraints.gridy = 2;
         getContentPane().add(osInfo,layoutConstraints);
 
+        layoutConstraints.gridx = 1;
+        layoutConstraints.gridy = 1;
+        layoutConstraints.gridheight = 2;
+        getContentPane().add(imageBanner,layoutConstraints);
+
     }
 
     public void displayAlert(){
 
-        this.setSize(550,450);
-        //this.setResizable(false);
+        this.setSize(900,500);
+        this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 
--- app/src/main/java/com/aquirozc/shortcutsmanager/util/Linker.java
+++ app/src/main/java/com/aquirozc/shortcutsmanager/util/Linker.java
@@ -17,7 +17,6 @@ public class Linker {
 
     public Linker(){
         super();
-        symLinkTarget = "/Volumes/Elements/Linktest";
     }
 
     public void createShortcut (Application application){
